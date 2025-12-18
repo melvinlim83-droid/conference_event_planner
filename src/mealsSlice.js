ee761c9dd3add456ac13abb5b2347ae21b0e8837
melvinlim83-droid <melvinlim83@gmail.com>
2025-12-18 06:39:34 -0500

    Filling in missing implementation for slice files

--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -4,8 +4,29 @@ import { createSlice } from '@reduxjs/toolkit';
 export const mealsSlice = createSlice({
   name: 'meals',
   initialState: [
+    {
+      name: "Breakfast",
+      cost: 50,
+      quantity: 0,
+    },
+    {
+      name: "Lunch",
+      cost: 60,
+      quantity: 0,
+    },
+    {
+      name: "High tea",
+      cost: 25,
+      quantity: 0,
+    },
+    {
+      name: "Dinner",
+      cost: 70,
+      quantity: 0,
+    },
    
   ],
+
   reducers: {
     toggleMealSelection: (state, action) => {
     },
